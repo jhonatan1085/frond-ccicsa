bcf091d3b55a8f22ef60a8b0626ecd6e25c7883f
Lord Zorel <marco.friz.p@gmail.com>
2024-08-23 13:01:12 -0400

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  URL_BACKEND: 'http://127.0.0.1:8000.test',
-  URL_SERVICIOS: 'http://127.0.0.1:8000/api',
-  URL_FRONTEND: 'http://localhost:4200/',
+  production: true,
+  URL_BACKEND: 'https://cicsa.situaweb.com',
+  URL_SERVICIOS: 'https://cicsa.situaweb.com/public/api',
+  URL_FRONTEND: 'https://cicsa.situaweb.com/',
 };
